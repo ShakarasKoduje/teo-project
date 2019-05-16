53afcbdbe4bea6b4df98e5e2b009392c8e5731da
czort <skruszonekrainy@gmail.com>
2019-05-16 22:16:18 +0200

    przygotowanie pod celery worker w docker-compose

--- teoproject/urls.py
+++ teoproject/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from sqlalchemy import create_engine
-import psycopg2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
 
